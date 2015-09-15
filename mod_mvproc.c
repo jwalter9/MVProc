4c6585a5932a5bb0173a827110b95d2d29d1773d
pebbleroller <pebbleroller@fbcfbddb-2d08-4972-ab3a-6651176e28ed>
2015-09-15 00:07:25 +0000

    removing leftover message from a failed integration with postgres

--- mod_mvproc.c
+++ mod_mvproc.c
@@ -330,7 +330,7 @@ static void generate_output(request_rec *r, modmvproc_config *cfg,
 	static const char *maybe_build_cache(cmd_parms *parms, void *mconfig, const char *arg){
 		modmvproc_config *cfg = ap_get_module_config(parms->server->module_config, &mvproc_module);
 		if(cfg->group == NULL)
-			return "mvprocCache directive must follow mvprocDbGroup or mvprocDbConnectString directive";
+			return "mvprocCache directive must follow mvprocDbGroup directive";
 		// Turn on caching ONLY if explicitly specified
 		if(arg[0] == 'N' || arg[0] == 'n') return NULL;
 		const char *cv = NULL;
